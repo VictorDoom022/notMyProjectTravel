fbd4ebcac4192a37517ba04394609803e0a2e525
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-06-17 16:14:41 +0800

    placeholder for navBar

--- views/index.php
+++ views/index.php
@@ -10,6 +10,23 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <!-- start of nav bar section -->
+    <nav class="navbar navbar-light navbar-expand-md bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand h1" href="index.php">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" aria-current="page" href="#">Facebook</a>
+                    <a class="nav-link" href="#">Twitter</a>
+                    <a class="nav-link" href="#">Whatever</a>
+                </div>
+            </div>
+            <button class="btn btn-outline-secondary btn-md" type="submit">Login</button>
+        </div>
+    </nav>
+    <!-- end of nav bar section -->
 </body>
 </html>
