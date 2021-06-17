9cd630edfb72106be636bea068511dc5bbc13636
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-06-17 22:49:11 +0800

    add admin panel to show all user

---
+++ views/adminPanel.php
@@ -0,0 +1,43 @@
+<?php
+require_once('../functions/connectDB.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">User Lists</h1>
+
+        <table class="table table-hover">
+            <tr class="table-dark">
+                <td>No.</td>
+                <td>Username</td>
+                <td>E-mail</td>
+            </tr>
+            <tr>
+            <?php
+                $sql = "SELECT * FROM users";
+                $result = mysqli_query($conn, $sql);
+                if(mysqli_num_rows($result) > 0){
+                    while($row = mysqli_fetch_assoc($result)){
+            ?>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['username']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+            <?php
+                    }
+                }
+            ?>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
