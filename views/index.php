ff33e4f509003873d2d15005334b28526ede2b22
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-06-17 17:17:59 +0800

    add footer section

--- views/index.php
+++ views/index.php
@@ -125,5 +125,33 @@
         </div>
     </div>
     <!-- end of popular tours section -->
+
+    <!-- start of footer section -->
+    <div class="container-fluid bg-secondary">
+        <div class="row">
+            <div class="col-md-3 text-white">
+                <p class="fw-bold">CompanyName</p>
+            </div>
+
+            <div class="col-md-3 text-white">
+                <p class="mb-0">Address: </p>
+                <p class="fst-italic">99, Jalan Wangi 99, Taman Sangat Wangi, 81200 Johor Bahru, Johor, Malaysia</p>
+            </div>
+
+            <div class="col-md-3 text-white">
+                <p class="mb-0">Office number: </p>
+                <p class="fst-italic">999</p>
+            </div>
+
+            <div class="col-md-3 text-white">
+                <p class="fst-italic">support info?</p>
+            </div>
+
+            <div class="col-md-12 text-center text-white">
+                <p class="fw-lighter">Copyright © 2021 MyCompany</p>
+            </div>
+        </div>
+    </div>
+    <!-- end of footer section -->
 </body>
 </html>
