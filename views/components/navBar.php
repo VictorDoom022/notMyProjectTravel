2aa9321c5611476706947343056baa73feb7e9af
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-06-17 17:39:15 +0800

    use icons instead of text & some minor fixes for navBar

--- views/components/navBar.php
+++ views/components/navBar.php
@@ -1,16 +1,22 @@
 <nav class="navbar navbar-light navbar-expand-md bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand h1" href="index.php">Navbar</a>
+        <a class="navbar-brand h1 mr-0 mb-0" href="index.php">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link" aria-current="page" href="#">Facebook</a>
-                <a class="nav-link" href="#">Twitter</a>
-                <a class="nav-link" href="#">Whatever</a>
+                <a class="nav-link" aria-current="page" href="#">
+                    <i class="bi bi-facebook"></i>
+                </a>
+                <a class="nav-link" href="#">
+                    <i class="bi bi-twitter"></i>
+                </a>
+                <a class="nav-link" href="#">
+                    <i class="bi bi-question-circle"></i>
+                </a>
             </div>
+            <button class="btn btn-outline-secondary btn-md" type="submit">Login</button>
         </div>
-        <button class="btn btn-outline-secondary btn-md" type="submit">Login</button>
     </div>
 </nav>
