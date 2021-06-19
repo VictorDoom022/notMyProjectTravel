779cd86d208c01cc61c62c37322cd016c2b6db44
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-06-19 22:00:28 +0800

    remove updown button for input type number

--- views/confirmOrder.php
+++ views/confirmOrder.php
@@ -26,6 +26,17 @@ if(mysqli_num_rows($result) > 0){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     <title>Document</title>
     <style>
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
     </style>
 </head>
 <body class="bg-light">
