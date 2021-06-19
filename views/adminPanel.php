1d0b484d45ccc91f2eb1bdf987a903d9116a5585
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2021-06-19 22:28:39 +0800

    display inquiries for admin

--- views/adminPanel.php
+++ views/adminPanel.php
@@ -176,6 +176,38 @@ require_once('../functions/connectDB.php');
             ?>
         </table>
     </div>
+
+    <div class="container">
+        <h1 class="text-center">Enquiry Lists</h1>
+
+        <table class="table table-hover table-responsive">
+            <tr class="table-dark">
+                <td>No.</td>
+                <td>Name</td>
+                <td>E-mail</td>
+                <td>Subect</td>
+                <td>Message</td>
+            </tr>
+            
+            <?php
+                $sql = "SELECT * FROM inquiries";
+                $result = mysqli_query($conn, $sql);
+                if(mysqli_num_rows($result) > 0){
+                    while($row = mysqli_fetch_assoc($result)){
+            ?>
+                <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['iqName']; ?></td>
+                    <td><?php echo $row['iqEmail']; ?></td>
+                    <td><?php echo $row['iqSubject']; ?></td>
+                    <td><?php echo $row['iqMessage']; ?></td>
+                </tr>
+            <?php
+                    }
+                }
+            ?>
+        </table>
+    </div>
 </body>
 <script>
 function deleteBooking(bookingID){
